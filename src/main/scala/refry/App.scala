d24b9003f4da65c39bdfd915b11a8e4b69e6330e
Adam Dąbrowski <contact@adamdabrowski.com>
2024-08-10 19:08:07 +0200

    minor: Add --help command

--- src/main/scala/refry/App.scala
+++ src/main/scala/refry/App.scala
@@ -2,15 +2,25 @@ package refry
 
 
 import cats.effect.{ExitCode, IO, IOApp}
-import cats.syntax.all.*
+import cats.effect.std.Console
 
 
 object App extends IOApp:
 
     override def run(args: List[String]): IO[ExitCode] =
         args match
-            case name :: Nil => IO.println(greet(name)).as(ExitCode.Success)
-            case _           => ExitCode.Error.pure[IO]
+            case "--help" :: Nil =>
+                Console[IO].print(help).as(ExitCode.Success)
+            case name :: Nil     =>
+                Console[IO].print(greet(name)).as(ExitCode.Success)
+            case _               =>
+                Console[IO].error(help).as(ExitCode.Error)
 
 
-    private[refry] def greet(name: String): String = s"Hello, $name!!!"
+    private[refry] def greet(name: String): String =
+        s"Hello, $name!!!"
+
+
+    private[refry] val help: String =
+        """|Usage: refry PATH...
+           |""".stripMargin
