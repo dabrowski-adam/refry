6024553eebae81a0ff2759ccb247d0f3b1041f6d
Adam Dąbrowski <contact@adamdabrowski.com>
2024-08-11 01:43:24 +0200

    minor: Append newline to CLI output

--- src/main/scala/refry/App.scala
+++ src/main/scala/refry/App.scala
@@ -11,14 +11,14 @@ object App extends IOApp:
     override def run(args: List[String]): IO[ExitCode] =
         args match
             case Help()      =>
-                Console[IO].print(help).as(ExitCode.Success)
+                Console[IO].println(help).as(ExitCode.Success)
             case path :: Nil =>
                 for
                     files <- tastyFiles(path)
-                    _     <- Console[IO].print(files.mkString("\n"))
+                    _     <- Console[IO].println(files.mkString("\n"))
                 yield ExitCode.Success
             case _           =>
-                Console[IO].error(help).as(ExitCode.Error)
+                Console[IO].errorln(help).as(ExitCode.Error)
 
 
     private object Help:
