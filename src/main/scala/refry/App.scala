832d19a4c8ab8fa5d53dc3ab5dbd2772e8521bf0
Adam Dąbrowski <contact@adamdabrowski.com>
2024-08-10 22:54:51 +0200

    minor: Expand valid --help commands

--- src/main/scala/refry/App.scala
+++ src/main/scala/refry/App.scala
@@ -9,18 +9,25 @@ object App extends IOApp:
 
     override def run(args: List[String]): IO[ExitCode] =
         args match
-            case "--help" :: Nil =>
+            case Help()      =>
                 Console[IO].print(help).as(ExitCode.Success)
-            case name :: Nil     =>
+            case name :: Nil =>
                 Console[IO].print(greet(name)).as(ExitCode.Success)
-            case _               =>
+            case _           =>
                 Console[IO].error(help).as(ExitCode.Error)
 
 
-    private[refry] def greet(name: String): String =
-        s"Hello, $name!!!"
+    private object Help:
+        def unapply(args: List[String]): Boolean =
+            args match
+                case Nil | ("--help" | "-help" | "help") :: Nil => true
+                case _                                          => false
 
 
     private[refry] val help: String =
         """|Usage: refry PATH...
            |""".stripMargin
+
+
+    private[refry] def greet(name: String): String =
+        s"Hello, $name!!!"
