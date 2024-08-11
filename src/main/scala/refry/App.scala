90e254a7646f8c6572f0de2267cb8ff5f1df660a
Adam Dąbrowski <contact@adamdabrowski.com>
2024-08-11 18:28:47 +0200

    minor: Associate .tasty files with .scala sources

--- src/main/scala/refry/App.scala
+++ src/main/scala/refry/App.scala
@@ -15,7 +15,9 @@ object App extends IOApp:
             case Paths(paths) =>
                 for
                     sources <- paths.parFlatTraverse(files)
-                    _       <- Console[IO].println(sources.mkString("\n"))
+                    groups   = group(sources)
+                    output   = groups.map((s, t) => s"$s ⇄ $t").mkString("\n")
+                    _       <- Console[IO].println(output)
                 yield ExitCode.Success
             case _            => Console[IO].errorln(help).as(ExitCode.Error)
 
@@ -47,3 +49,23 @@ object App extends IOApp:
             .filter(file => file.extName == ".scala" || file.extName == ".tasty")
             .compile
             .toList
+
+
+    private[refry] def group(files: List[Path]): Map[Path, Path] =
+        files
+            .groupBy: file =>
+                // TODO: Get .scala path from @SourceFile in .tasty
+                val bareFileName = Path(file.fileName.toString.dropRight(6))
+
+                file.normalize.absolute.parent match
+                    case Some(dir) => dir.resolve(bareFileName)
+                    case _         => file.fileName
+            .flatMap: (_key, value) =>
+                val maybeScala = value.find(_.extName == ".scala")
+                val maybeTasty = value.find(_.extName == ".tasty")
+
+                (maybeScala, maybeTasty) match
+                    case (Some(source), Some(tasty)) => Map(source -> tasty)
+                    case _                           => Map.empty
+
+end App
